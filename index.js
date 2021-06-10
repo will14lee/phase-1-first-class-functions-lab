933079ee9f9dfc356714175f5231a293a3f3c22d
will14lee <will14lee@gmail.com>
2021-06-10 00:52:11 -0400

    commit tree d4ee8b57f8e39638aab04dfa2dbb2907e3a1ce20

--- index.js
+++ index.js
@@ -12,49 +12,14 @@ const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers]
    
 // console.log(selectingDrivers);    
 
-//     describe('createFareMultiplier()', function () {
-//       it('returns a function', function () {
-//         const fareMultiplier = createFareMultiplier(2);
-  
-//         expect(fareMultiplier).to.be.a('function');
-//       });
-  
-//       it('should multiply a given value using the created multiplier', function () {
-//         const fareQuintupler = createFareMultiplier(5);
-  
-//         expect(fareQuintupler(5)).to.eql(25);
-//       });
-//     });
-function createFareMultiplier(price=5){
-    function fareQuintupler(){
-        price*5};
-        return fareQuintupler(5)
+function createFareMultiplier(price){
+    return function (multiplier){
+        return price*multiplier};
     };
 
-console.log(createFareMultiplier());
-// const fareQuintupler= createFareMultiplier(5,5)
-// console.log(fareQuintupler);
+let fareDoubler= createFareMultiplier(2)
 
-//     describe('fareDoubler()', function () {
-//       it('is a function', function () {
-//         expect(fareDoubler).to.be.a('function');
-//       });
-  
-//       it('doubles fares', function () {
-//         expect(fareDoubler(10)).to.eql(20);
-//       });
-//     });
-  
-//     describe('fareTripler()', function () {
-//       it('is a function', function () {
-//         expect(fareTripler).to.be.a('function');
-//       });
-  
-//       it('triples fares', function() {
-//         expect(fareTripler(12)).to.eql(36);
-//       });
-//     });
-  
+let fareTripler= createFareMultiplier(3)
 //     describe('selectDifferentDrivers(arrayOfDrivers, function)', function () {
 //       it('returns the first two drivers when passed returnFirstTwoDrivers() as the second argument', function () {
 //         expect(selectDifferentDrivers(drivers, returnFirstTwoDrivers)).to.eql(['Sally', 'Bob']);
@@ -65,4 +30,7 @@ console.log(createFareMultiplier());
 //       });
 //     });
 //   });
-  
+let selectDifferentDrivers= function arrayofDrivers (drivers){
+    return (drivers ? returnFirstTwoDrivers() : returnLastTwoDrivers()) 
+}
+console.log(selectDifferentDrivers(false));
